ed6bbb804a9ec59a5e2e0921169a919ac5f6e22e
Zhihao Bai <rero@Zhihaos-MacBook-Pro.local>
2024-08-12 17:31:48 -0700

    adding code for converting images

---
+++ images.py
@@ -0,0 +1,19 @@
+from PIL import Image, ImageFilter
+
+
+# img = Image.open('./astro.jpg')
+# print(img.size)
+# img.show()
+# img.thumbnail((400,400))
+# img.show()
+# img.save('thumbnail.jpg')
+# print(img.size)
+
+# img = Image.open('./Pokedex/pikachu.jpg')
+# grey_img = img.convert('L')
+# blur = grey_img.filter(ImageFilter.BLUR)
+# box = (0,0,640,320)
+# region = grey_img.crop(box)
+# region.save("region.png", 'png')
+# blur.show()
+# grey_img.resize((150,640)).show()
